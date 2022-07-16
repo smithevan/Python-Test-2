8ab87d87bedfbb3ec0dd4dd999142ad622b2978c
Evan Smith <smith.m.evan@gmail.com>
2022-07-16 20:13:47 +0100

    nodes adding to hash map over loops

--- consensus.py
+++ consensus.py
@@ -3,15 +3,12 @@ import random
 from platform import node
 
 nodes = {}
-node_number = 0
-
 
 def create_node (nodes, node_number):
-    print('Create new node (y/n)?  ')
-    print('Type "q" to quit.')
+    print('Create new node (y/n/q)?  ')
     node_creation = input()
     if node_creation == 'y':
-        node_number += 1
+        print(node_number)
         node_weight = random.randint(0, 999)
         nodes.update({node_number: node_weight})
         print('Node ' + str(node_number) + ' created.')
@@ -20,18 +17,22 @@ def create_node (nodes, node_number):
     elif node_creation == 'n':
         print('Node not created.')
         return True
-    elif node_creation == 'q':
-        
+    elif node_creation == 'q': 
         return False
     
 
+
 def main():
+    node_number = 0
     while create_node(nodes, node_number):
         print(nodes)
+        node_number += 1
+        print('xx:' + str(node_number))
     else:
         print('Process terminated')
     
 main()
+
 
 
 
