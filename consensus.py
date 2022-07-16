491c10922398fad59330cf64aef4cfc566f6bd4c
Evan Smith <smith.m.evan@gmail.com>
2022-07-16 19:25:42 +0100

    concatenate node number and weights

--- consensus.py
+++ consensus.py
@@ -2,25 +2,27 @@ import math
 import random
 from platform import node
 
-nodes = []
+nodes = {}
+node_number = 0
 
-def create_node (nodes):
+def create_node (nodes, node_number):
     print('Create new node (y/n)?  ')
     print('Type "q" to quit.')
     node_creation = input()
     if node_creation == 'y':
+        node_number += 1
         node_weight = random.randint(0, 999)
-        nodes.append(node_weight)
-        print('Node created.')
-        print(node_weight)
+        #nodes.append(node_weight)
+        print('Node ' + str(node_number) + ' created.')
+        print('Node weight: ' + str(node_weight))
     elif node_creation == 'n':
         print('Node not created.')
     elif node_creation == 'q':
         print('Process terminated')
-    return node_creation
+    
 
 def main():
-    create_node(nodes)
+    create_node(nodes, node_number)
     print(nodes)
 main()
 
