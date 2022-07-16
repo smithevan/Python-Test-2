341fcaf72579c49fee9991741ad509ceb25d38d3
Evan Smith <smith.m.evan@gmail.com>
2022-07-16 22:12:45 +0100

    preliminary node calculation using multiple sequential guesses. Not ready yet, commented out.

--- consensus.py
+++ consensus.py
@@ -9,6 +9,19 @@ def check_lead_node(nodes):
     lead_node_key = max(nodes, key=nodes.get)
     print('Node ' + str(lead_node_key) + ' is the lead node.')
 
+#def calculate_node_solution(position):
+#    steps = {}
+#    step_number = 0
+#    next_guess = random.randint(0, 999)
+ #   difference = abs(position - next_guess)
+ #   steps.update({step_number: difference})
+  #  while difference >= 5:
+   #     next_guess = random.randint(position, difference)
+    #    steps.update({step_number: difference})
+
+    
+
+
 def create_node (nodes, node_number):
     print('Create new node (y/n/q)?  ')
     node_creation = input()
