2b0184ee46bb0cbc4d13b3aa64769df9d75c2889
Evan Smith <smith.m.evan@gmail.com>
2022-07-16 19:18:30 +0100

    push node value to nodes array

--- consensus.py
+++ consensus.py
@@ -2,22 +2,26 @@ import math
 import random
 from platform import node
 
-def create_node ():
+nodes = []
+
+def create_node (nodes):
     print('Create new node (y/n)?  ')
     print('Type "q" to quit.')
     node_creation = input()
     if node_creation == 'y':
         node_weight = random.randint(0, 999)
+        nodes.append(node_weight)
         print('Node created.')
         print(node_weight)
     elif node_creation == 'n':
         print('Node not created.')
     elif node_creation == 'q':
         print('Process terminated')
+    return node_creation
 
 def main():
-    create_node()
-
+    create_node(nodes)
+    print(nodes)
 main()
 
 
